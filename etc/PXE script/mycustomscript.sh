115752abdcdd7ae6dc377ea192f7ab6607848178
VoxSecundus <millard64@hotmail.co.uk>
2019-08-30 14:09:51 +0100

    Image setup now converts zip to binary before sending

--- etc/PXE script/mycustomscript.sh
+++ etc/PXE script/mycustomscript.sh
@@ -8,25 +8,22 @@ make
 make install
 gem install bundler
 git clone -b develop https://github.com/openflighthpc/flight-hunter.git /root/hunter
-git clone https://github.com/openflighthpc/flight-inventory.git /root/inventory
 git clone https://github.com/openflighthpc/flight-inventory-data-gatherer.git /root/gatherer
 chmod +x /root/hunter/bin
 chmod +x /root/gatherer/build
-chmod +x /root/inventory/bin
 cd /root/inventory
 bundle install
 mkdir var/store/default
 cd /root/hunter
 bundle install
-bin/hunter.rb modify-ip 10.10.0.1
 touch /root/hunter.sh
 cat <<EOT >> /root/hunter.sh
 #!/bin/bash
 cd /root/gatherer/build
 ./gather-data-bundled.sh payload
 chmod 755 /tmp/payload.zip
-/root/inventory/bin/inventory import /tmp/payload.zip
-/root/hunter/bin/hunter.rb send --file /root/inventory/var/store/default/payload.yaml
+od -An -vtx1 /tmp/payload.zip > /tmp/payload.txt
+/root/hunter/bin/hunter.rb send --file /tmp/payload.txt
 EOT
 chmod +x /root/hunter.sh
 chmod +X /etc/rc.d/rc.local
