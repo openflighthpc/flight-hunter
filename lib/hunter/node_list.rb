0215cb8f04443c3e2ff295a3f0eca41fa5336f40
VoxSecundus <millard64@hotmail.co.uk>
2022-09-23 17:24:18 +0100

    Create node and nodelist classes

---
+++ lib/hunter/node_list.rb
@@ -0,0 +1,67 @@
+#==============================================================================
+# Copyright (C) 2022-present Alces Flight Ltd.
+#
+# This file is part of Flight Hunter.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# Flight Hunter is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Hunter. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Hunter, please visit:
+# https://github.com/openflighthpc/flight-hunter
+#==============================================================================
+
+module Hunter
+  class NodeList
+
+    class << self
+      def load(filepath)
+        filename = filepath.split("/").compact.last
+        raise "Node list #{filename} doesn't exist" if !File.file?(filepath)
+        new(filepath)
+      end
+    end
+
+    def to_yaml
+      nodes.to_yaml
+    end
+
+    def save
+      File.open(filepath, 'w+') { |f| f.write(to_yaml) }
+    end
+
+    attr_reader :filepath
+    attr_accessor :nodes
+
+    private
+
+    def initialize(filepath)
+      @filepath = filepath
+      @nodes = [].tap do |l|
+        list = YAML.load_file(filepath) || {}
+        list.each do |node|
+          l << Node.new(
+            node['id'],
+            node['hostname'],
+            node['ip'],
+            node['payload']
+          )
+        end
+      end
+    end
+  end
+end
