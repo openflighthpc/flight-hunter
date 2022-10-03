134f4fc5de035ca68178ec8822bd5b0c24ce2819
VoxSecundus <millard64@hotmail.co.uk>
2022-10-03 17:08:11 +0100

    Support PID file tracking

--- lib/hunter/version.rb
+++ lib/hunter/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-hunter
 #==============================================================================
 module Hunter
-  VERSION = '0.1.0'
+  VERSION = '0.2.0-rc1'
 end
