c875bca802bb341a4a5b6690e13cbc356eaf579f
Matt Ruddy <matt@calcwise.com>
2023-02-03 15:03:42 +0000

    Fix typo

--- lib/hunter/commands/send.rb
+++ lib/hunter/commands/send.rb
@@ -108,7 +108,7 @@ module Hunter
         hostname = Socket.gethostname
 
         # Find MAC of the relevant interface
-        host = @options.broadcast ? broadcast_address || target_host
+        host = @options.broadcast ? broadcast_address : target_host
         interface = `ip route get #{host} | sed -n -e 's/^.*dev //p' | awk '{print $1}'`.chomp
         if interface == "lo"
           interface = `ip route get 8.8.8.8 | sed -n -e 's/^.*dev //p' | awk '{print $1}'`.chomp
