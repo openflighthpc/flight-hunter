9fa89adc2c7283a998b7f4b4b6720d2291d26c68
Jack Millard <millard64@hotmail.co.uk>
2023-04-16 16:48:12 +0100

    Allow auto-parse to set as a config option

--- etc/config.yml.ex
+++ etc/config.yml.ex
@@ -1,27 +1,31 @@
 ---
-# Host to send data to when running client
+# Host to send data to when running client (optional)
 # target_host:
 #
-# Port to listen/send over. This must match on both server and client.
+# Port to listen/send over. This must match on both server and client. (required)
 # port:
 #
-# Mode to use when autorun
+# Mode to use when autorun (optional)
 # autorun_mode:
 #
-# Automatically include self when hunting
+# Automatically include self when hunting (optional)
 # include_self: false
 #
-# Command to use to generate content
+# Command to use to generate content (optional)
 # content_command:
 #
-# Allow existing nodes when hunting
+# Allow existing nodes when hunting (optional)
 # allow_existing: false
 #
-# Key used to authenticate data
+# Key used to authenticate data (required)
 # auth_key: flight-hunter
 #
-# Broadcast address to use when sending via a UDP broadcast
+# Broadcast address to use when sending via a UDP broadcast (optional)
 # broadcast_address: 127.0.255.255
 #
-# Command used to run the Flight Profile executable
+# Command used to run the Flight Profile executable (optional)
 # profile_command:
+#
+# Attempt to automatically parse nodes when hunted, if they match the given
+# regular expression (optional)
+# auto_parse: /expression/
