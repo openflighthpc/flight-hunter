901648903b2a51f02c9d5740eebbb1ac8f116ae7
VoxSecundus <millard64@hotmail.co.uk>
2023-04-24 16:59:18 +0100

    Bump version

--- lib/hunter/version.rb
+++ lib/hunter/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-hunter
 #==============================================================================
 module Hunter
-  VERSION = '0.3.2-rc2'
+  VERSION = '0.3.2-rc3'
 end
