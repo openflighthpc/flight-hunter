0d2e7c6eb2b16ee0769708415bbc064fa04c50ea
Jack Millard <millard64@hotmail.co.uk>
2023-05-12 16:44:59 +0100

    Bump version

--- lib/hunter/version.rb
+++ lib/hunter/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-hunter
 #==============================================================================
 module Hunter
-  VERSION = '0.3.2-rc3'
+  VERSION = '0.4.0'
 end
