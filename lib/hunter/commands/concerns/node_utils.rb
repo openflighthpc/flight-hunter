914e36da242821749e53537536a19bd9282e3a15
Jack Millard <millard64@hotmail.co.uk>
2024-01-19 11:25:17 +0000

    Move useful NodeUtils methods to classes

--- lib/hunter/commands/concerns/node_utils.rb
+++ lib/hunter/commands/concerns/node_utils.rb
@@ -1,41 +1,64 @@
 # frozen_string_literal: true
 
+require_relative '../../node_list'
+
 module Hunter
   module Commands
     module NodeUtils
       class InvalidRangeError < StandardError; end
 
-      def read_nodes(str)
-        statements = str.split(',')
+      class CLIParser
+        def expand(str)
+          statements = str.split(',')
 
-        collections = statements.map do |statement|
-          # Expand bracket format
-          expand_brackets(statement)
+          collections = statements.map do |statement|
+            # Expand bracket format
+            expand_brackets(statement)
+          end
+
+          collections.reduce(:+).uniq.compact
         end
 
-        collections.reduce(:+).uniq
+        def expand_brackets(str)
+          contents = str[/\[.*\]/]
+
+          return [str] if contents.nil?
+
+          left = str[/[^\[]*/]
+          right = str[/].*/][1..-1]
+
+          unless contents.match(/^\[[0-9]+-[0-9]+\]$/)
+            raise InvalidRangeError, "'#{contents}' is not of the format [START-END]."
+          end
+
+          nums = contents[1..-2].split('-')
+
+          unless nums.first.to_i <  nums.last.to_i
+            raise InvalidRangeError, "'#{contents}' has a start index that is greater than its end index."
+          end
+
+          (nums.first..nums.last).map do |index|
+            left + index + right
+          end
+        end
       end
 
-      def expand_brackets(str)
-        contents = str[/\[.*\]/]
-
-        return [str] if contents.nil?
-
-        left = str[/[^\[]*/]
-        right = str[/].*/][1..-1]
-
-        unless contents.match(/^\[[0-9]+-[0-9]+\]$/)
-          raise InvalidRangeError, "'#{contents}' is not of the format [START-END]."
+      class NodeFetcher
+        def initialize(buffer: false)
+          case buffer
+          when true
+            @list = NodeList.load(Config.node_buffer)
+            @search_field = :id
+          else
+            @list = NodeList.load(Config.node_list)
+            @search_field = :label
+          end
         end
 
-        nums = contents[1..-2].split('-')
+        attr_reader :list, :search_field
 
-        unless nums.first.to_i <  nums.last.to_i
-          raise InvalidRangeError, "'#{contents}' has a start index that is greater than its end index."
-        end
-
-        (nums.first..nums.last).map do |index|
-          left + index + right
+        def scan(arr)
+          arr.map { |n| @list.find(@search_field => n) }.compact
         end
       end
     end
